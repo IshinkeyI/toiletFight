97179ad275e015eed47bcd71172bbbcd05d58cfa
Shinkey <ilgammakh@mail.ru>
2023-11-21 19:10:32 +0300

    create ui scene. StartGame button logic.

---
+++ Assets/Scripts/UI/MainMenuUI/StartGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace UI.MainMenuUI
+{
+	public class StartGame : MonoBehaviour
+	{
+		[SerializeField] private Button startGameButton;
+
+		private void Awake()
+		{
+			startGameButton.onClick.AddListener(() =>
+			{
+				SceneManager.LoadScene(sceneBuildIndex: 1);
+			});
+		}
+	}
+}
